b9f3eaadce6b4278762d6ec85a6089f1c21d654c
juanborssotto <jborssotto@gmail.com>
2018-11-18 18:33:31 -0300

    Cambio app routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,8 +3,7 @@ import { RouterModule, Routes } from "@angular/router";
 const routes: Routes = [
     { path: "autenticacion", loadChildren: "./autenticacion/autenticacion.module#AutenticacionModule" },
     { path: "reclamos", loadChildren: "./reclamos/reclamos.module#ReclamosModule" },
-    // { path: "**", redirectTo: "autenticacion", pathMatch: "full" }
-    { path: "**", redirectTo: "reclamos", pathMatch: "full" }
+    { path: "**", redirectTo: "autenticacion", pathMatch: "full" }
 ];
 
 export const appRoutes = RouterModule.forRoot(routes);
